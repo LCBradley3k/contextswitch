f2923057614b078d504d31f02832bbf0da2ec4ae
LCBradley3k <bradleymitchellis@gmail.com>
2021-02-26 23:45:46 -0800

    add absolute header bar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="App__Header">
+        <h1>Context <span>Switch</span></h1>
+        <h2>The middleware for your life.</h2>
+      </div>
       <div className="App__TaskContainer">
         <Task name="work"/>
         <Task name="lunch"/>
